a98c385aa2808bae07148735bbafb1c76e653f0e
zeeenku <enajjachi.zakariaa@gmail.com>
2025-01-22 18:41:50 +0100

    react children bug solved

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { default as ZPwaInstallDialog } from '@/z-pwa-install-dialog';
-export { default as ZPwaInstallDialogTrigger } from '@/z-pwa-install-dialog-trigger';
+// export { default as ZPwaInstallDialog } from '@/z-pwa-install-dialog';
+// export { default as ZPwaInstallDialogTrigger } from '@/z-pwa-install-dialog-trigger';
 export { default as ZPwaInstallInstruc } from '@/z-pwa-install-instruc';
