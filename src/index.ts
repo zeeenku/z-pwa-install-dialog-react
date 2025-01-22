fb2388ebef9a09b8d092ca52260126fd4d4a3756
zeeenku <enajjachi.zakariaa@gmail.com>
2025-01-22 22:13:25 +0100

    dialog component added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import "./globals.css";
 export { default as ZPwaInstallDialog } from '@/z-pwa-install-dialog';
-export { default as ZPwaInstallDialogBtn } from '@/z-pwa-install-dialog-btn';
-export { default as ZPwaInstallInstruc } from '@/z-pwa-install-instruc';
+// export { default as ZPwaInstallInstruc } from '@/z-pwa-install-instruc';
+// export { default as ZPwaInstallDialogBtn } from '@/z-pwa-install-dialog-btn';
