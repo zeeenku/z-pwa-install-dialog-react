d25996a08a08dbdf33eda6fe4445858a9e50e4ce
Zakariaa Enajjachi <enajjachi.zakariaa@gmail.com>
2025-05-25 14:05:02 +0100

    package via create-tsup initalised

---
+++ new-proj/src/index.tsx
@@ -0,0 +1,3 @@
+export { default as ZPwaInstallDialogBtn } from './z-pwa-install-dialog-btn';
+export { default as ZPwaInstallInstruc } from './z-pwa-install-instruc';
+export { default as ZPwaInstallDialog } from './z-pwa-install-dialog';
